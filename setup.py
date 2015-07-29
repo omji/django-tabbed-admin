1d152e66f5fe01205a2941cbb1401444d6034898
Guillaume Pousseo <guillaumepousseo@revsquare.com>
2015-07-28 18:12:07 -1000

    release 0.0.3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ else:
 
 setup(
     name='django-tabbed-admin',
-    version='0.0.2',
+    version='0.0.3',
     author='Guillaume Pousseo',
     author_email='guillaumepousseo@revsquare.com',
     description='Easily add tabs to django admin forms.',
